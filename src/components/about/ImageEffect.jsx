38af03f9d6fffa4cf3e111b0e72a34767ea1d59e
sunny-zerror <sunny.zerror@gmail.com>
2026-08-04 19:13:56 +0530

    useref import fix

--- src/components/about/ImageEffect.jsx
+++ src/components/about/ImageEffect.jsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
